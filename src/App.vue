0e18544cc5a0f18005b99dc10d58700e91e012af
harshan <harshan.l2020ece@sece.ac.in>
2023-06-08 17:20:55 +0530

    desktop view compleated

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,77 @@
 <template >
-  <Search class="card"/>
-  <WeatherBox class="card"/>
+  <!-- <Search class="card"/> -->
+  <WeatherBox class="card" :val=val v-cloak/>
   <div class="small">
-    <SmallBox/>
-    <SmallBox/>
-    <SmallBox/>
-    <SmallBox/>
-    <SmallBox/>
-    <SmallBox/>
-
+    <SmallBox :val="val" cnt="1" />
+    <SmallBox :val="val" cnt="2" />
+    <SmallBox :val="val" cnt="3" />
+    <SmallBox :val="val" cnt="4" />
+    <SmallBox :val="val" cnt="5" />
+    <SmallBox :val="val" cnt="6" />
+    <SmallBox :val="val" cnt="7" />
   </div>
 </template>
 
 <script>
-import Search from './components/Search.vue';
+// import Search from './components/Search.vue';
 import WeatherBox from'./components/wether.vue';
 import SmallBox from './components/Small.vue';
 export default {
   name: 'App',
   components: {
-    Search,WeatherBox,SmallBox
+    // Search,
+    WeatherBox,SmallBox
+  },
+  data(){
+    return {
+      coordinates:
+      {
+        lat:0,
+        lng:0
+      },
+      val:{},
+    }
+  },
+  created() {
+    this.$getLocation()
+    .then((coordinates) => {
+        this.coordinates=coordinates;
+        // console.log(this.coordinates);
+        this.fetchweather();
+      })
+    .catch((error) => {
+        console.log(error);
+      });
+      
+    },
+  methods:
+  {
+    fetchweather(){
+    //   console.log(this.coordinates)
+      let url=`https://api.weatherapi.com/v1/forecast.json?key=62ce7928966f48269ed91923230806&q=${this.coordinates.lat},${this.coordinates.lng}&days=8`;
+      fetch(url)
+      .then((resp)=>{
+        if(!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+        })
+      .then(this.seeres)
+      .catch(console.error);
+      },
+      seeres(res)
+      {
+        console.log(res)
+        this.val=res
+      },
+      timeConverter(date){
+        var c=date.split("-")
+        var nd= c[2]+"-"+c[1]+"-"+c[0]
+        return nd;
+    },
+      getimg()
+      {
+        return new URL(this.icon,import.meta.url)
+      }
+
   }
 }
 </script>
--- src/components/Small.vue
+++ src/components/Small.vue
@@ -1,9 +1,8 @@
 <template>
 <div class="sbox">
-    <div class="simg">
-    </div>
+    <img class="simg" :src="icon">
     <div class="stext">
-        <h3>{{ degree }}<sup>o</sup></h3>
+        <h3>{{ Math.round(temp) }}°</h3>
     </div>
     <div class="stext">
         <h3>{{ date }}</h3>
@@ -11,16 +10,73 @@
 </div>
 
 </template>
-
 <script>
+
 export default
 {
     name:'SmallBox',
     props:{
-        degree:String,
-        date:String,
-
+        cnt:Number,
     },
+    data()
+    {
+        return {
+        coordinates:
+      {
+        lat:0,
+        lng:0
+      },
+      val:{},
+      temp:{},
+      date:{},
+      icon:{},
+    }
+  },
+  created() {
+    this.$getLocation()
+    .then((coordinates) => {
+        this.coordinates=coordinates;
+        this.fetchweather();
+      })
+    .catch((error) => {
+        console.log(error);
+      });
+      
+    },
+  methods:
+  {
+    fetchweather(){
+    //   console.log(this.coordinates)
+      let url=`https://api.weatherapi.com/v1/forecast.json?key=62ce7928966f48269ed91923230806&q=${this.coordinates.lat},${this.coordinates.lng}&days=8&aqi=no&alerts=no`;
+      fetch(url)
+      .then((resp)=>{
+        if(!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+        })
+      .then(this.seeres)
+      .catch(console.error);
+      },
+      seeres(res)
+      {
+        console.log(res)
+        this.temp=res.forecast.forecastday[this.cnt].day.avgtemp_c
+        this.date=this.timeConverter(res.forecast.forecastday[this.cnt].date)
+        this.icon=res.forecast.forecastday[this.cnt].day.condition.icon
+        this.icon=("https:".concat(this.icon))
+        console.log(this.icon)
+
+      },
+      timeConverter(date){
+        var c=date.split("-")
+        var nd= c[2]+"-"+c[1]+"-"+c[0]
+        return nd;
+    },
+      getimg()
+      {
+        return new URL(this.icon,import.meta.url)
+      }
+
+  }
 }
 </script>
 
@@ -28,7 +84,7 @@ export default
 .sbox
 {
     min-width: 10vw;
-    max-width: fit-content;
+    max-width: 10vw;
     min-height: 30vh;
     background-color: #b9ccda6f;
     border-radius: 20px;
@@ -43,10 +99,10 @@ export default
 .simg
 {
     min-width: 7vw;
-    max-width: 17vw;
+    max-width: 7vw;
     min-height: 13vh;
     margin-top: 2vh;
-    background-color: white;
+    /* background-color: white; */
     display: flex;
     justify-content: center;
     align-items: center;
